35971f2b4ff2a5fd02613ecf45ccf4c0e09c6d3c
cldme <ioncld97@gmail.com>
2020-04-08 00:14:00 +0200

    updated simulation code to include infection and recovered rates

--- workers.py
+++ workers.py
@@ -1,14 +1,18 @@
 import networkx as nx
 import numpy as np
 import pandas as pd
+import random
 
-def simulate_infection(seed, data, N_nodes):
+def check_measures(time, row, isolated_nodes, delay):
+    _is_isolated_node_1 = True if isolated_nodes.get(row[0]) is not None else False
+    _is_isolated_node_2 = True if isolated_nodes.get(row[1]) is not None else False
+    return (time < delay) or (not _is_isolated_node_1 and not _is_isolated_node_2)
+
+def simulate_infection(seed, data, nodes, isolated_nodes, delay, infection_rate, recover_time):
     # set of infected nodes (grows with every new infected node in the network)
     infected_nodes = set({seed})
     # list with number of infected nodes for every timestamp
     infected_nodes_time = list()
-    # set of edges to be removed after simulation is done for current timestamp
-    remove_edges = list()
     # store rank of seed node
     rank = None
     # store number of initial nodes infected by seed node (feature used to later rank nodes)
@@ -16,9 +20,9 @@ def simulate_infection(seed, data, N_nodes):
     initial_done = False
     # time at which seed infected first other node
     initial_infection_time = None
-
-    # generate the graph
-    graph = nx.Graph()
+    # dict storing initial infection times
+    infection_time = dict()
+    infection_time[seed] = 1
 
     # initial starting time
     t = 1
@@ -28,27 +32,33 @@ def simulate_infection(seed, data, N_nodes):
             time_diff = row[2] - t
             infected_nodes_time.append(len(infected_nodes) * time_diff)
             t = row[2]
-            graph.clear()
-            # graph.remove_edges_from(remove_edges)
-            # remove_edges.clear()
+            _remove = set()
+            for _node in infected_nodes:
+                if infection_time[_node] + recover_time < t:
+                    _remove.update([_node])
+                    isolated_nodes[_node] = True
+            infected_nodes -= _remove
             # stop checking for initial infected nodes
             if initial_infections != 0 and not initial_done:
                 initial_done = True
-        # build graph by adding edges (simulate infection / info spreading)
-        graph.add_edge(row[0], row[1])
-        # keep track of edges added at current timestamp (used when moving to next timestamp for removing them)
-        # remove_edges.append((row[0], row[1]))
-        if row[0] in infected_nodes or row[1] in infected_nodes:
-            infected_nodes.update([row[0], row[1]])
-            # logic for ranking a node's influence
-            if len(infected_nodes) > 0.8 * N_nodes and rank is None:
-                rank = t
-        # check for initial infected nodes
-        if (row[0] == seed or row[1] == seed) and not initial_done:
-            initial_infections += 1
-        # check whether the seed infects the initial node
-        if initial_infection_time is None and (row[0] == seed or row[1] == seed):
-            initial_infection_time = t
+        # check if measures are in place (and a connection does occur)
+        if check_measures(t, row, isolated_nodes, delay):
+            # simulate infection spreading
+            if (row[0] in infected_nodes or row[1] in infected_nodes) and random.random() < infection_rate:
+                infected_nodes.update([row[0], row[1]])
+                if infection_time.get(row[0]) is None:
+                    infection_time[row[0]] = t
+                if infection_time.get(row[1]) is None:
+                    infection_time[row[1]] = t
+                # logic for ranking a node's influence
+                if len(infected_nodes) > 0.8 * nodes and rank is None:
+                    rank = t
+            # check for initial infected nodes
+            if (row[0] == seed or row[1] == seed) and not initial_done:
+                initial_infections += 1
+            # check whether the seed infects the initial node
+            if initial_infection_time is None and (row[0] == seed or row[1] == seed):
+                initial_infection_time = t
     infected_nodes_time.append(len(infected_nodes))
     
     return infected_nodes_time, rank, initial_infections, initial_infection_time
